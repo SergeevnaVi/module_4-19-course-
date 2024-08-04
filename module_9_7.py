a53a1855473c59602e34ca0f5c5977ee7e8ecfb4
Violetta Borschova <borshovavioletta2006@gmail.com>
2024-08-05 00:41:55 +0300

    #module 9

---
+++ module_9_7.py
@@ -0,0 +1,18 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        if res % res == 0 and res != 0:
+            print('Простое')
+        else:
+            print('Составное')
+        return res
+    return wrapper
+
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+
+result = sum_three(2, 3, 6)
+print(result)
