ae4f5b1a8fbb46eab4a091e7d1daf6672a33b0b7
Violetta Borschova <borshovavioletta2006@gmail.com>
2024-08-11 19:16:13 +0300

    #module 9

--- module_9_7.py
+++ module_9_7.py
@@ -1,10 +1,16 @@
 def is_prime(func):
     def wrapper(*args, **kwargs):
         res = func(*args, **kwargs)
-        if res % res == 0 and res != 0:
+        if res == 2:
             print('Простое')
-        else:
+        if res <= 1:
             print('Составное')
+        for n in range(2, res):
+            if res % n == 0:
+                print('Составное')
+                break
+        else:
+            print('Простое')
         return res
     return wrapper
 
@@ -16,3 +22,7 @@ def sum_three(a, b, c):
 
 result = sum_three(2, 3, 6)
 print(result)
+result2 = sum_three(5, 5, 5)
+print(result2)
+result3 = sum_three(5, 3, 9)
+print(result3)
